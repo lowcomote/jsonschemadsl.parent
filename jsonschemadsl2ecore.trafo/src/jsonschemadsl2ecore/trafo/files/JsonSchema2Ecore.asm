7d99b0a3a20c2f9901fb4f7b99287bb993059d31
alessandro.colantoni <alessandro.colantoni@jku.at>
2021-02-11 17:45:49 +0100

    moved helper in utils

--- jsonschemadsl2ecore.trafo/src/jsonschemadsl2ecore/trafo/files/JsonSchema2Ecore.asm
+++ jsonschemadsl2ecore.trafo/src/jsonschemadsl2ecore/trafo/files/JsonSchema2Ecore.asm
@@ -80,56 +80,24 @@
 		<constant value="19:4-19:40"/>
 		<constant value="18:3-20:4"/>
 		<constant value="link"/>
-		<constant value="isTypeObject"/>
-		<constant value="MMM!ObjectSchema;"/>
-		<constant value="0"/>
-		<constant value="keywordDefinition"/>
-		<constant value="TypeSchemaDefinition"/>
-		<constant value="J.oclIsTypeOf(J):J"/>
-		<constant value="B.not():B"/>
-		<constant value="20"/>
-		<constant value="CJ.including(J):CJ"/>
-		<constant value="typeAnyOf1"/>
-		<constant value="J.oclIsUndefined():J"/>
-		<constant value="J.not():J"/>
-		<constant value="31"/>
-		<constant value="EnumLiteral"/>
-		<constant value="&quot;object&quot;"/>
-		<constant value="J.=(J):J"/>
-		<constant value="B.or(B):B"/>
-		<constant value="24:2-24:6"/>
-		<constant value="24:2-24:24"/>
-		<constant value="24:44-24:51"/>
-		<constant value="24:64-24:87"/>
-		<constant value="24:44-24:88"/>
-		<constant value="24:2-24:89"/>
-		<constant value="25:26-25:33"/>
-		<constant value="25:26-25:44"/>
-		<constant value="25:26-25:61"/>
-		<constant value="25:22-25:61"/>
-		<constant value="24:2-25:62"/>
-		<constant value="26:22-26:29"/>
-		<constant value="26:22-26:40"/>
-		<constant value="26:43-26:56"/>
-		<constant value="26:22-26:56"/>
-		<constant value="24:2-26:57"/>
-		<constant value="keyword"/>
 		<constant value="__matchObjectSchemaToEClass"/>
 		<constant value="ObjectSchema"/>
 		<constant value="J.isTypeObject():J"/>
+		<constant value="B.not():B"/>
+		<constant value="31"/>
 		<constant value="objectSchema"/>
 		<constant value="eClass"/>
 		<constant value="EClass"/>
-		<constant value="45:34-45:46"/>
-		<constant value="45:34-45:61"/>
-		<constant value="47:3-47:22"/>
+		<constant value="26:34-26:46"/>
+		<constant value="26:34-26:61"/>
+		<constant value="29:3-29:22"/>
 		<constant value="__applyObjectSchemaToEClass"/>
 		<constant value="eClassifiers"/>
-		<constant value="49:3-49:13"/>
-		<constant value="49:3-49:26"/>
-		<constant value="49:42-49:48"/>
-		<constant value="49:3-49:49"/>
-		<constant value="48:2-50:3"/>
+		<constant value="31:3-31:13"/>
+		<constant value="31:3-31:26"/>
+		<constant value="31:42-31:48"/>
+		<constant value="31:3-31:49"/>
+		<constant value="30:2-32:3"/>
 	</cp>
 	<field name="1" type="2"/>
 	<field name="3" type="4"/>
@@ -380,87 +348,11 @@
 		</localvariabletable>
 	</operation>
 	<operation name="79">
-		<context type="80"/>
-		<parameters>
-		</parameters>
-		<code>
-			<pushf/>
-			<push arg="15"/>
-			<push arg="9"/>
-			<new/>
-			<push arg="15"/>
-			<push arg="9"/>
-			<new/>
-			<load arg="81"/>
-			<get arg="82"/>
-			<iterate/>
-			<store arg="23"/>
-			<load arg="23"/>
-			<push arg="83"/>
-			<push arg="53"/>
-			<findme/>
-			<call arg="84"/>
-			<call arg="85"/>
-			<if arg="86"/>
-			<load arg="23"/>
-			<call arg="87"/>
-			<enditerate/>
-			<iterate/>
-			<store arg="23"/>
-			<load arg="23"/>
-			<get arg="88"/>
-			<call arg="89"/>
-			<call arg="90"/>
-			<call arg="85"/>
-			<if arg="91"/>
-			<load arg="23"/>
-			<call arg="87"/>
-			<enditerate/>
-			<iterate/>
-			<store arg="23"/>
-			<load arg="23"/>
-			<get arg="88"/>
-			<push arg="92"/>
-			<push arg="9"/>
-			<new/>
-			<dup/>
-			<push arg="93"/>
-			<set arg="41"/>
-			<call arg="94"/>
-			<call arg="95"/>
-			<enditerate/>
-		</code>
-		<linenumbertable>
-			<lne id="96" begin="7" end="7"/>
-			<lne id="97" begin="7" end="8"/>
-			<lne id="98" begin="11" end="11"/>
-			<lne id="99" begin="12" end="14"/>
-			<lne id="100" begin="11" end="15"/>
-			<lne id="101" begin="4" end="20"/>
-			<lne id="102" begin="23" end="23"/>
-			<lne id="103" begin="23" end="24"/>
-			<lne id="104" begin="23" end="25"/>
-			<lne id="105" begin="23" end="26"/>
-			<lne id="106" begin="1" end="31"/>
-			<lne id="107" begin="34" end="34"/>
-			<lne id="108" begin="34" end="35"/>
-			<lne id="109" begin="36" end="41"/>
-			<lne id="110" begin="34" end="42"/>
-			<lne id="111" begin="0" end="44"/>
-		</linenumbertable>
-		<localvariabletable>
-			<lve slot="1" name="112" begin="10" end="19"/>
-			<lve slot="1" name="112" begin="22" end="30"/>
-			<lve slot="1" name="112" begin="33" end="43"/>
-			<lve slot="0" name="21" begin="0" end="44"/>
-		</localvariabletable>
-	</operation>
-	<operation name="113">
 		<context type="7"/>
 		<parameters>
 		</parameters>
 		<code>
-			<push arg="114"/>
+			<push arg="80"/>
 			<push arg="53"/>
 			<findme/>
 			<push arg="54"/>
@@ -468,9 +360,9 @@
 			<iterate/>
 			<store arg="23"/>
 			<load arg="23"/>
-			<call arg="115"/>
-			<call arg="85"/>
-			<if arg="91"/>
+			<call arg="81"/>
+			<call arg="82"/>
+			<if arg="83"/>
 			<getasm/>
 			<get arg="1"/>
 			<push arg="56"/>
@@ -480,12 +372,12 @@
 			<push arg="49"/>
 			<pcall arg="57"/>
 			<dup/>
-			<push arg="116"/>
+			<push arg="84"/>
 			<load arg="23"/>
 			<pcall arg="59"/>
 			<dup/>
-			<push arg="117"/>
-			<push arg="118"/>
+			<push arg="85"/>
+			<push arg="86"/>
 			<push arg="62"/>
 			<new/>
 			<pcall arg="63"/>
@@ -494,27 +386,27 @@
 			<enditerate/>
 		</code>
 		<linenumbertable>
-			<lne id="119" begin="7" end="7"/>
-			<lne id="120" begin="7" end="8"/>
-			<lne id="121" begin="23" end="28"/>
+			<lne id="87" begin="7" end="7"/>
+			<lne id="88" begin="7" end="8"/>
+			<lne id="89" begin="23" end="28"/>
 		</linenumbertable>
 		<localvariabletable>
-			<lve slot="1" name="116" begin="6" end="30"/>
+			<lve slot="1" name="84" begin="6" end="30"/>
 			<lve slot="0" name="21" begin="0" end="31"/>
 		</localvariabletable>
 	</operation>
-	<operation name="122">
+	<operation name="90">
 		<context type="7"/>
 		<parameters>
 			<parameter name="23" type="67"/>
 		</parameters>
 		<code>
 			<load arg="23"/>
-			<push arg="116"/>
+			<push arg="84"/>
 			<call arg="68"/>
 			<store arg="32"/>
 			<load arg="23"/>
-			<push arg="117"/>
+			<push arg="85"/>
 			<call arg="69"/>
 			<store arg="70"/>
 			<load arg="70"/>
@@ -522,19 +414,19 @@
 			<getasm/>
 			<get arg="5"/>
 			<load arg="70"/>
-			<set arg="123"/>
+			<set arg="91"/>
 		</code>
 		<linenumbertable>
-			<lne id="121" begin="8" end="9"/>
-			<lne id="124" begin="10" end="10"/>
-			<lne id="125" begin="10" end="11"/>
-			<lne id="126" begin="12" end="12"/>
-			<lne id="127" begin="10" end="13"/>
-			<lne id="128" begin="10" end="13"/>
+			<lne id="89" begin="8" end="9"/>
+			<lne id="92" begin="10" end="10"/>
+			<lne id="93" begin="10" end="11"/>
+			<lne id="94" begin="12" end="12"/>
+			<lne id="95" begin="10" end="13"/>
+			<lne id="96" begin="10" end="13"/>
 		</linenumbertable>
 		<localvariabletable>
-			<lve slot="3" name="117" begin="7" end="13"/>
-			<lve slot="2" name="116" begin="3" end="13"/>
+			<lve slot="3" name="85" begin="7" end="13"/>
+			<lve slot="2" name="84" begin="3" end="13"/>
 			<lve slot="0" name="21" begin="0" end="13"/>
 			<lve slot="1" name="78" begin="0" end="13"/>
 		</localvariabletable>
--- jsonschemadsl2ecore.trafo/src/jsonschemadsl2ecore/trafo/files/JsonSchema2EcoreUtils.asm
+++ jsonschemadsl2ecore.trafo/src/jsonschemadsl2ecore/trafo/files/JsonSchema2EcoreUtils.asm
@@ -5,6 +5,45 @@
 		<constant value="main"/>
 		<constant value="A"/>
 		<constant value="self"/>
+		<constant value="isTypeObject"/>
+		<constant value="MMM!ObjectSchema;"/>
+		<constant value="Sequence"/>
+		<constant value="#native"/>
+		<constant value="0"/>
+		<constant value="keywordDefinition"/>
+		<constant value="1"/>
+		<constant value="TypeSchemaDefinition"/>
+		<constant value="MM"/>
+		<constant value="J.oclIsTypeOf(J):J"/>
+		<constant value="B.not():B"/>
+		<constant value="20"/>
+		<constant value="CJ.including(J):CJ"/>
+		<constant value="typeAnyOf1"/>
+		<constant value="J.oclIsUndefined():J"/>
+		<constant value="J.not():J"/>
+		<constant value="31"/>
+		<constant value="EnumLiteral"/>
+		<constant value="&quot;object&quot;"/>
+		<constant value="name"/>
+		<constant value="J.=(J):J"/>
+		<constant value="B.or(B):B"/>
+		<constant value="4:2-4:6"/>
+		<constant value="4:2-4:24"/>
+		<constant value="4:44-4:51"/>
+		<constant value="4:64-4:87"/>
+		<constant value="4:44-4:88"/>
+		<constant value="4:2-4:89"/>
+		<constant value="5:26-5:33"/>
+		<constant value="5:26-5:44"/>
+		<constant value="5:26-5:61"/>
+		<constant value="5:22-5:61"/>
+		<constant value="4:2-5:62"/>
+		<constant value="6:22-6:29"/>
+		<constant value="6:22-6:40"/>
+		<constant value="6:43-6:56"/>
+		<constant value="6:22-6:56"/>
+		<constant value="4:2-6:57"/>
+		<constant value="keyword"/>
 	</cp>
 	<operation name="1">
 		<context type="2"/>
@@ -16,6 +55,82 @@
 		</linenumbertable>
 		<localvariabletable>
 			<lve slot="0" name="3" begin="0" end="-1"/>
+		</localvariabletable>
+	</operation>
+	<operation name="4">
+		<context type="5"/>
+		<parameters>
+		</parameters>
+		<code>
+			<pushf/>
+			<push arg="6"/>
+			<push arg="7"/>
+			<new/>
+			<push arg="6"/>
+			<push arg="7"/>
+			<new/>
+			<load arg="8"/>
+			<get arg="9"/>
+			<iterate/>
+			<store arg="10"/>
+			<load arg="10"/>
+			<push arg="11"/>
+			<push arg="12"/>
+			<findme/>
+			<call arg="13"/>
+			<call arg="14"/>
+			<if arg="15"/>
+			<load arg="10"/>
+			<call arg="16"/>
+			<enditerate/>
+			<iterate/>
+			<store arg="10"/>
+			<load arg="10"/>
+			<get arg="17"/>
+			<call arg="18"/>
+			<call arg="19"/>
+			<call arg="14"/>
+			<if arg="20"/>
+			<load arg="10"/>
+			<call arg="16"/>
+			<enditerate/>
+			<iterate/>
+			<store arg="10"/>
+			<load arg="10"/>
+			<get arg="17"/>
+			<push arg="21"/>
+			<push arg="7"/>
+			<new/>
+			<dup/>
+			<push arg="22"/>
+			<set arg="23"/>
+			<call arg="24"/>
+			<call arg="25"/>
+			<enditerate/>
+		</code>
+		<linenumbertable>
+			<lne id="26" begin="7" end="7"/>
+			<lne id="27" begin="7" end="8"/>
+			<lne id="28" begin="11" end="11"/>
+			<lne id="29" begin="12" end="14"/>
+			<lne id="30" begin="11" end="15"/>
+			<lne id="31" begin="4" end="20"/>
+			<lne id="32" begin="23" end="23"/>
+			<lne id="33" begin="23" end="24"/>
+			<lne id="34" begin="23" end="25"/>
+			<lne id="35" begin="23" end="26"/>
+			<lne id="36" begin="1" end="31"/>
+			<lne id="37" begin="34" end="34"/>
+			<lne id="38" begin="34" end="35"/>
+			<lne id="39" begin="36" end="41"/>
+			<lne id="40" begin="34" end="42"/>
+			<lne id="41" begin="0" end="44"/>
+		</linenumbertable>
+		<localvariabletable>
+			<lve slot="1" name="42" begin="10" end="19"/>
+			<lve slot="1" name="42" begin="22" end="30"/>
+			<lve slot="1" name="42" begin="33" end="43"/>
+			<lve slot="0" name="3" begin="0" end="44"/>
 		</localvariabletable>
 	</operation>
 </asm>
