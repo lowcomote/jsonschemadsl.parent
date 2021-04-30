303e0dbebc524458d12f28774103dd67aefc9a68
alessandro.colantoni <alessandro.colantoni@jku.at>
2021-04-30 12:43:35 +0200

    dirty commit

--- jsonschemadsl2ecore.trafo.opt/src/jsonschemadsl2ecore/trafo/opt/runner/JsonSchemaMToEcoreRunner.java
+++ jsonschemadsl2ecore.trafo.opt/src/jsonschemadsl2ecore/trafo/opt/runner/JsonSchemaMToEcoreRunner.java
@@ -19,7 +19,9 @@ public class JsonSchemaMToEcoreRunner implements IApplication{
 	public final static String OUTPUT_MODEL = "output";
 	public final static String ECORE_MODEL = JSON_SCHEMA_MODEL_NAME + ".ecore";
 	public final static String JSON_GRAMMAR_MODEL = JSON_SCHEMA_MODEL_NAME + "-grammar.jsongrammar";
+	public final static String OCL_MODEL = JSON_SCHEMA_MODEL_NAME + ".ocl";
 	public final static String TRACE_MODEL = JSON_SCHEMA_MODEL_NAME + "-trace-model.xmi";
+	
 	
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
@@ -27,6 +29,7 @@ public class JsonSchemaMToEcoreRunner implements IApplication{
 				getModelFile(JSONSCHEMA_MODEL),
 				getOutputModelFile(ECORE_MODEL), 
 				getOutputModelFile(JSON_GRAMMAR_MODEL), 
+				getOutputModelFile(OCL_MODEL),
 				TRACE_MODEL);		
 		return null;
 	}
